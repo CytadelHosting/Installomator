76f69fb754ff06fb5eab41c7782d0e780e47803e
KarimAltavia <k.bachekour@altavia-aura.com>
2023-02-20 11:15:14 +0100

    modification du script par s.roche, dmg herbergé sur Cytadel

--- fragments/labels/forticlientvpn.sh
+++ fragments/labels/forticlientvpn.sh
@@ -1,7 +1,9 @@
-forticlientvpn)
+ forticlientvpn)
     name="FortiClient"
-    type="pkg"
-    #Trouver un Url sans code mais avec le nom de l'app
-    downloadURL="https://share.altavia-aura.com/fdl/zgflwbJMnQlY" 
+    type="pkgInDmg"
+    #Modification du script fait par Sylavain Roche et le DMG herbergé sur Cytadel
+    appNewVersion=$(curl https://www.cytadel.fr/wp-content/uploads/FortiClient-version.txt/ 2>/dev/null | grep "version"|awk '{ print $2 }')
+    downloadURL="https://www.cytadel.fr/wp-content/uploads/FortiClient.dmg"
     expectedTeamID="AH4XFXJ7DK"
+    pkgName="Install.mpkg"
     ;;

26ec596baa2630eb02705eb27b37c5cb0af0efbf
KarimAltavia <k.bachekour@altavia-aura.com>
2023-02-20 10:54:47 +0100

    ajout d'un commentaire sur l'Url

--- fragments/labels/forticlientvpn.sh
+++ fragments/labels/forticlientvpn.sh
@@ -1,6 +1,7 @@
 forticlientvpn)
     name="FortiClient"
     type="pkg"
+    #Trouver un Url sans code mais avec le nom de l'app
     downloadURL="https://share.altavia-aura.com/fdl/zgflwbJMnQlY" 
     expectedTeamID="AH4XFXJ7DK"
     ;;

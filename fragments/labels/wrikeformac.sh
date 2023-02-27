91b805bdbb4aaf891a0bd99da7d82fd010973a5f
Sylvain Roche <5450786+niavlysAO@users.noreply.github.com>
2023-02-27 22:00:53 +0100

    dynamically get latest version from changelog

--- fragments/labels/wrikeformac.sh
+++ fragments/labels/wrikeformac.sh
@@ -2,11 +2,10 @@ wrikeformac)
     name="Wrike for Mac"
     type="dmg"
     appNewVersion="4.0.6"
+    appNeVersion=$(curl -fs "https://www.wrike.com/frontend/electron-app/changelog.json" | grep -Eio '"version": "(.*)' | head -n 1 | cut -d '"' -f 4)
     if [[ $(arch) == i386 ]]; then
-        #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.latest.dmg"      # valide pour arch i386
-        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.v${appNewVersion}.dmg"      # pour la coherence avec silicon, on hardcode le numéro de vesrion
+        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.v${appNewVersion}.dmg"
     elif [[ $(arch) == arm64 ]]; then
-        #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.latest.dmg"  # ne marche pas avec latest, il faut obligatoirement un numéro de version précis
         downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.v${appNewVersion}.dmg"
     fi
     expectedTeamID="BD3YL53XT4"
